e304e53bca751681105c08cd346532f50ffd4060
Андрей Волков <andrew.volkov.dev@gmail.com>
2019-05-13 21:49:35 +0300

    Version 3.0 after another Artem's remarks

--- JS-homework-3/js/script.js
+++ JS-homework-3/js/script.js
@@ -3,11 +3,19 @@ let secondNumber = getNumber();
 let mathOperator = getMathOperator();
 
 function getNumber () {
-    return prompt("Enter a number");
+    let number = prompt("Enter a number");
+    while (number === null || isNaN(number) || number.match(/\s/) || number.length === 0) {
+        number = prompt("Enter is incorect. Enter a corect number");
+    }
+    return number;
 }
 
 function getMathOperator () {
-    return prompt("Enter the math operator from +, -, *, /");
+    let operator = prompt("Enter the math operator from +, -, *, /");
+    while (!(operator === "+" || operator === "-" || operator === "*" || operator === "/")) {
+        operator = prompt("Enter is incorect. Enter on of the symbol from +, -, *, /");
+    }
+    return operator;
 }
 
 function mathOperation(firstNum, secondNum, operator) {
@@ -27,77 +35,4 @@ function mathOperation(firstNum, secondNum, operator) {
     }
 }
 
-while (firstNumber === null || isNaN(Number(firstNumber)) || firstNumber.match(/\s/) || firstNumber.length === 0) {
-    firstNumber = getNumber();
-}
-
-while (secondNumber === null || isNaN(Number(secondNumber)) || secondNumber.match(/\s/) || secondNumber.length === 0) {
-    secondNumber = getNumber();
-}
-
-while (!(mathOperator !== "+" || mathOperator !== "-" || mathOperator !== "*" || mathOperator !== "/")) {
-    mathOperator = getMathOperator();
-}
-
 mathOperation(firstNumber, secondNumber, mathOperator);
-
-
-
-
-
-
-// OLD VERSION
-
-// let firstNumber = prompt("Enter first number");
-// let secondNumber = prompt("Enter second number");
-// let mathOperator = prompt("Enter the math operator from +, -, *, /");
-//
-// function incorrectNumberEnter(userNumberEnter) {
-//     if (userNumberEnter === null || isNaN(Number(userNumberEnter)) || userNumberEnter.match(/\s/) || userNumberEnter.length === 0) {
-//         return true;
-//     }
-//     else {
-//         return false;
-//     }
-// }
-//
-// function correctMathOperator(operator) {
-//     if (operator === "+" || operator === "-" || operator === "*" || operator === "/" ) {
-//         return true;
-//     }
-//     else {
-//         return false;
-//     }
-// }
-//
-// while (incorrectNumberEnter(firstNumber) || incorrectNumberEnter(secondNumber) || !(correctMathOperator(mathOperator))) {
-//     if (incorrectNumberEnter(firstNumber)) {
-//         firstNumber = prompt("Your first enter is incorrect, please, enter a number...");
-//     }
-//     if (incorrectNumberEnter(secondNumber)) {
-//         secondNumber = prompt("Your second enter is incorrect, please, enter a number...");
-//     }
-//     if (!(correctMathOperator(mathOperator))) {
-//         mathOperator = prompt("Your enter isn't a math operator from +, -, *, /. Please, try again...");
-//     }
-// }
-//
-// function mathOperation(firstNum, secondNum, operator) {
-//     switch (operator) {
-//         case "+":
-//             console.log(firstNum + secondNum);
-//             break;
-//         case "-":
-//             console.log(firstNum - secondNum);
-//             break;
-//         case "*":
-//             console.log(firstNum * secondNum);
-//             break;
-//         case "/":
-//             console.log(firstNum / secondNum);
-//             break;
-//     }
-// }
-//
-// mathOperation(firstNumber, secondNumber, mathOperator);
-
