168f1e57238cfa82d9ef13d67a907dcdf4825576
Андрей Волков <andrew.volkov.dev@gmail.com>
2019-05-22 13:44:08 +0300

    End of HW 16 (optional). Edited after menthor check

--- JS-homework-16/js/script.js
+++ JS-homework-16/js/script.js
@@ -1,16 +1,6 @@
 let userNumber = prompt("Enter a number higher or equal 0 to find it factorial");
 
-function findFactorial(num) {
-    if (num == 0) {
-        return 1;
-    }
-    if (num !== 1) {
-        return num * findFactorial(num - 1);
-    }
-    else {
-        return num;
-    }
-}
+let findFactorial = num => num > 1 ? num * findFactorial(num - 1) : 1;
 
 while (userNumber === null || userNumber === "" || isNaN(Number(userNumber)) || userNumber < 0) {
     userNumber = prompt("Prohibited value. Enter a number higher or equal 0", userNumber);
