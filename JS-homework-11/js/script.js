af97369853f5360027ce288728a93a5686b43bce
Андрей Волков <andrew.volkov.dev@gmail.com>
2019-06-04 19:48:50 +0300

    HW11 done

--- JS-homework-11/js/script.js
+++ JS-homework-11/js/script.js
@@ -2,9 +2,8 @@ document.addEventListener("keydown", highLightButton);
 
 function highLightButton(event) {
     const btnsColection = document.getElementsByClassName("btn");
-
     for (let i = 0; i < btnsColection.length; i++) {
-        if (btnsColection[i].innerText === event.key || btnsColection[i].innerText === event.key.toUpperCase()) {
+        if (btnsColection[i].innerText.toLowerCase() === event.key.toLowerCase()) {
             for (let i = 0; i < btnsColection.length; i++){
                 btnsColection[i].classList.remove("blue-btn");
             }
