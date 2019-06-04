bb2fbb2a3267bddadb391a7d38334e2ebe29ad44
Андрей Волков <andrew.volkov.dev@gmail.com>
2019-06-04 11:50:18 +0300

    v.0.2 Almost finish

--- JS-homework-10/js/script.js
+++ JS-homework-10/js/script.js
@@ -1,24 +1,59 @@
-const showPassBtns = document.getElementsByClassName("fa-eye");
+const showPassBtns = document.getElementsByClassName("fas");
+const confirmBtn = document.getElementById("submit");
+
 
 for (let i = 0; i < showPassBtns.length; i++) {
-    showPassBtns[i].addEventListener("mousedown", showPassword);
+    showPassBtns[i].addEventListener("mousedown", changeInputType);
 }
 
-function showPassword() {
-    this.previousElementSibling.setAttribute("type", "text");
+confirmBtn.addEventListener("click", checkPass);
 
-    this.classList.remove("fa-eye");
-    this.classList.add("fa-eye-slash");
+function changeInputType() {
+    let inputSibling = this.previousElementSibling;
+    switch (inputSibling.type) {
+        case "text":
+            inputSibling.type = "password";
+            break;
+        case "password":
+            inputSibling.type = "text";
+            break;
+    }
 
-    let hidePassBtns = document.getElementsByClassName("fa-eye-slash");
-
-    for (let i = 0; i < hidePassBtns.length; i++) {
-        hidePassBtns[i].addEventListener("mousedown", hidePassword);
+    if(this.classList.contains("fa-eye")) {
+        this.classList.remove("fa-eye");
+        this.classList.add("fa-eye-slash");
+    }
+    else {
+        this.classList.remove("fa-eye-slash");
+        this.classList.add("fa-eye");
     }
 }
 
-function hidePassword() {
-    this.previousElementSibling.setAttribute("type", "password");
-    this.classList.remove("fa-eye-slash");
-    this.classList.add("fa-eye");
+function checkPass(event) {
+    event.preventDefault();
+    const closeCross = document.getElementById("closeModal");
+    const modalWindow = document.getElementById("modal");
+    const errorSection = document.getElementById("error-block");
+
+    closeCross.addEventListener("click", closeModal);
+
+    function closeModal() {
+        modalWindow.classList.remove("modal-active");
+    }
+
+    const inputFields = document.querySelectorAll("input[type='password']");
+
+    if (inputFields[0].value === inputFields[1].value) {
+        clearError();
+        modalWindow.classList.add("modal-active");
+    }
+    else {
+        clearError();
+        errorSection.innerHTML = "<p class='error'>Нужно ввести одинаковые значения</p>";
+    }
+
+    function clearError() {
+        errorSection.innerHTML = "";
+    }
 }
+
